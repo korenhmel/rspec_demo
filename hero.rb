df220dcef56d05a75e80e2fc79b44ac88b4bf0c5
Koren <koren.hmel@gmail.com>
2016-09-12 16:21:11 +0300

    Add hero entity

--- hero.rb
+++ hero.rb
@@ -1,3 +1,35 @@
 class Hero
+ def initialize(name, health = 100)
+ @name = name.capitalize
+ @health = health
+ end
+  def name
+   @name
+  end
+ def drink_tea
+  power_up
+ end
+ def drink_vodka
+  power_down(3)
+ end
+ def info
+  "name: #{@name} health: #{@health}"
+ end
+ private
+  def power_up(times = 1)
+   @health+= 10 * times
+  end
+
+  def power_down(times = 1)
+   @health-= 10 * times
+  end
+
+  def health
+   @health
+  end
 
 end
+
+hero = Hero.new 'luke'
+hero.drink_tea
+puts hero.info
