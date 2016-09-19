4bbc0584b0f2ff5b25319bea3688af834e4b2120
Koren <koren.hmel@gmail.com>
2016-09-19 18:34:19 +0300

    Add Car class

---
+++ car.rb
@@ -0,0 +1,20 @@
+class Car
+  MILES_PER_GALLON = 20
+  attr_reader :fuel
+  def initialize
+    @fuel = 0
+  end
+
+  def add_fuel amount
+    @fuel+= amount
+  end
+
+  def range
+    0
+ # distance = @fuel * MILES_PER_GALLON
+  end
+end
+
+car = Car.new
+car.add_fuel 21
+puts "Distance the car can go is #{car.range}"
