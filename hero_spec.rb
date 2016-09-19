1e11c141ac1fac4c6737a471fda84f72be929565
Koren <koren.hmel@gmail.com>
2016-09-19 14:10:01 +0300

    Refactoring test

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,33 +1,37 @@
 require './hero'
 
 describe Hero do
-  it "has a capitalized name" do
-  hero = Hero.new('mike')
+  before do
+    @hero = Hero.new('mike')
+  end
 
-  expect(hero.name).to eql 'Mike' # hero.name == 'Mike'
+  it "has a capitalized name" do
+  # hero = Hero.new('mike')
+
+  expect(@hero.name).to eql 'Mike' # hero.name == 'Mike'
   end
 
   it "can power up " do
-    hero = Hero.new('mike')
+    # hero = Hero.new('mike')
 
-    expect(hero.power_up).to eql 110
+    expect(@hero.power_up).to eql 110
   end
 
   it "can drink vodka" do
-    hero = Hero.new('mike')
+    # hero = Hero.new('mike')
 
-    expect(hero.drink_vodka).to eql 70
+    expect(@hero.drink_vodka).to eql 70
   end
 
   it "can power down" do
-    hero = Hero.new('mike')
+    # hero = Hero.new('mike')
 
-    expect(hero.power_down).to eql 90
+    expect(@hero.power_down).to eql 90
   end
 
   it "displayes full hero info" do
-    hero = Hero.new('mike')
+    # hero = Hero.new('mike')
 
-    expect(hero.info).to eql 'name: Mike health: 100'
+    expect(@hero.info).to eql 'name: Mike health: 100'
   end
 end
